be97b677a0bce568cefcc9dafd9a7b2636d81ec1
Yonga <netmdy@gmail.com>
2018-07-06 17:47:13 +0900

    :D

---
+++ bitcamp-web-03/src/main/java/bitcamp/pms/listener/ContextLoaderListener.java
@@ -0,0 +1,23 @@
+package bitcamp.pms.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import bitcamp.pms.dao.MemberDao;
+
+@WebListener
+public class ContextLoaderListener implements ServletContextListener{
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("ContextLoaderListener 실행 ㅇㅅㅇ");
+        MemberDao memberDao = new MemberDao(
+                "jdbc:mysql://13.209.8.213:3306/studydb","study","1111");
+        
+        ServletContext sc = sce.getServletContext();
+        sc.setAttribute("memberDao", memberDao);
+    }
+
+}
