82c886f0fc0f7a4bf4bc51acf34be3693cb4949a
Yonga <netmdy@gmail.com>
2018-08-01 18:05:20 +0900

    phonegap 추가

---
+++ bitcamp-phonegap-app01/www/js/common.js
@@ -0,0 +1,1 @@
+var serverApiAddr = "http://192.168.0.54:8080/bitcamp-web-18";
--- bitcamp-web-18/src/main/java/bitcamp/pms/controller/json/MemberController.java
+++ bitcamp-web-18/src/main/java/bitcamp/pms/controller/json/MemberController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import bitcamp.pms.domain.Member;
 import bitcamp.pms.service.MemberService;
  
+// @CrossOrigin
 @RestController
 @RequestMapping("/member")
 public class MemberController {
